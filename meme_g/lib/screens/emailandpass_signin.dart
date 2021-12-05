0721fc4d7a014b0e173f7a92a38792e9d83e434e
AmanB-2003 <90694309+AmanB-2003@users.noreply.github.com>
2021-12-06 01:55:22 +0530

    sign in screen and email-pass screen ui changed

--- meme_g/lib/screens/emailandpass_signin.dart
+++ meme_g/lib/screens/emailandpass_signin.dart
@@ -14,44 +14,83 @@ class _EmailandPassState extends State<EmailandPass> {
   var authObject = new Auth();
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child : Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          child: TextField(
-              decoration: InputDecoration(hintText: "Enter EmailId "),
-              onChanged: (email) {
-                setState(() {
-                  emailid = email;
-                });
-              }),
-        ),
-        Container(
-          child: TextField(
-            decoration: InputDecoration(hintText: "Enter Password"),
-            onChanged: (password) {
-              setState(() {
-                upassword = password.trim();
-              });
-            },
-            obscureText: true,
-          ),
-        ),
-        ElevatedButton(
-            onPressed: () async {
-              dynamic resultuser =
-                  await authObject.EmailandpassSignIn(emailid, upassword);
-              if (resultuser == null) {
-                print("sign in failed");
-              } else {
-                Navigator.pushNamed(context, Homescreen.route);
-                print(resultuser.uid);
-              }
-            },
-            child: Text("Let's Sign-in"))
-      ],
-    )
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('Sign In'),
+      ),
+       body: Card(
+           elevation: 5, 
+           shape: RoundedRectangleBorder(
+            borderRadius:  BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),  ),),
+            
+             child: Container(
+               padding: EdgeInsets.all(30),
+             child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+               Container(
+                 padding: EdgeInsets.all(10),
+                  height: 300,
+                  width: double.infinity,
+                  child: Image.asset(
+                    'assets/Mobile login-pana.png',  
+                  ),
+               ),
+          
+              TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Email Id ",
+                    
+                    ),
+                  onChanged: (email) {
+                    setState(() {
+                      emailid = email;
+                    });
+                  }),
+          
+         
+              TextField(
+                decoration: InputDecoration(hintText: "Enter Password"),
+                onChanged: (password) {
+                  setState(() {
+                    upassword = password.trim();
+                  });
+                },
+                obscureText: true,
+              ),
+           SizedBox(
+             height: 40.0,
+           ),
+
+          ElevatedButton(
+                style : ButtonStyle(
+                 padding : MaterialStateProperty.all(const EdgeInsets.all(5)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(15),
+                      bottom: Radius.circular(15)),
+                    )
+                  )   ,
+                  ),
+                onPressed: () async {
+                  dynamic resultuser =
+                      await authObject.EmailandpassSignIn(emailid, upassword);
+                  if (resultuser == null) {
+                    print("sign in failed");
+                  } else {
+                    Navigator.pushNamed(context, Homescreen.route);
+                    print(resultuser.uid);
+                  }
+                },
+                
+                child: Text("Let's Sign-in"))
+        ],
+      ),
+      ),
+           ),
     );
+    
   }
 }
