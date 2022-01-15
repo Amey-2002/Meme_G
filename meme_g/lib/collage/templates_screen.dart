7072f4a30403129cbc1baf8254a799a6921fbc15
Shubhams-2002 <sssalunkhe_b20@ce.vjti.ac.in>
2022-01-15 19:34:06 +0530

    search to select template
    errors-
    -blank screen before pressing search button
    -unable to load all templates

--- meme_g/lib/collage/templates_screen.dart
+++ meme_g/lib/collage/templates_screen.dart
@@ -21,71 +21,215 @@ class TemplatesScreen extends StatefulWidget {
   State<TemplatesScreen> createState() => _TemplatesScreenState();
 }
 
+  List<String> MemeName = [];
+  List<String> MemeURL = [];
+
 class _TemplatesScreenState extends State<TemplatesScreen> {
   late CollectionReference templatesRef;
-
-  User? user = FirebaseAuth.instance.currentUser;
 
   @override
   void initState() {
     super.initState();
-    if (user != null) {
-      templatesRef = FirebaseFirestore.instance
-          .collection('Templates');
-    } else {
-      templatesRef = FirebaseFirestore.instance.collection('Users');
-    }
+    templatesRef = FirebaseFirestore.instance.collection('Templates');
+    templatesRef.get().then((querySnapshot) {
+      for (var result in querySnapshot.docs) {
+        MemeName.add(result.get('name'));
+        MemeURL.add(result.get('url'));
+      }
+    });
+    _isSearching = false;
+  }
+
+  Widget appBarTitle = Text(
+    "Search Example",
+    style: TextStyle(color: Colors.white),
+  );
+  Icon icon = Icon(
+    Icons.search,
+    color: Colors.white,
+  );
+  final globalKey = GlobalKey<ScaffoldState>();
+  final TextEditingController _controller = TextEditingController();
+  late bool _isSearching;
+  String _searchText = "";
+  List searchresult = [];
+
+  _TemplatesScreenState() {
+    _controller.addListener(() {
+      if (_controller.text.isEmpty) {
+        setState(() {
+          _isSearching = false;
+          _searchText = "";
+        });
+      } else {
+        setState(() {
+          _isSearching = true;
+          _searchText = _controller.text;
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    _handleSearchStart();
+
+    // templatesRef = FirebaseFirestore.instance.collection('Templates');
+    // templatesRef.get().then((querySnapshot) {
+    //   for (var result in querySnapshot.docs) {
+    //     MemeName.add(result.get('name'));
+    //     MemeURL.add(result.get('url'));
+    //   }
+    // });
+
+    print(MemeURL);
+    print(MemeName);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Templates')),
-      body: StreamBuilder<QuerySnapshot>(
-        stream:
-            templatesRef.snapshots(),
-        builder: (context, snapshot) {
-          return !snapshot.hasData
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Container(
-                  padding: EdgeInsets.all(4),
-                  child: GridView.builder(
-                    itemCount: snapshot.data?.docs.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2),
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: EdgeInsets.all(3),
-                        child: InkWell(
-                          child: FadeInImage.memoryNetwork(
+      //key: globalKey,
+      appBar: AppBar(centerTitle: true, title: appBarTitle, actions: <Widget>[
+        IconButton(
+          icon: icon,
+          onPressed: () {
+            setState(() {
+              if (this.icon.icon == Icons.search) {
+                this.icon = Icon(
+                  Icons.close,
+                  color: Colors.white,
+                );
+                this.appBarTitle = TextField(
+                  controller: _controller,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search, color: Colors.white),
+                      hintText: "Search...",
+                      hintStyle: TextStyle(color: Colors.white)),
+                  onChanged: searchOperation,
+                );
+                _handleSearchStart();
+              } else {
+                _handleSearchEnd();
+              }
+            });
+          },
+        ),
+      ]),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Flexible(
+              child: searchresult.length != 0 || _controller.text.isNotEmpty
+                  ? GridView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: searchresult.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.all(3),
+                          child: InkWell(
+                            child: FadeInImage.memoryNetwork(
                               fit: BoxFit.cover,
                               placeholder: kTransparentImage,
-                              image: snapshot.data?.docs[index].get('url')),
-                          onTap: () async {
-                            Navigator.pop(context, snapshot.data?.docs[index].get('url'));
-                            // showDialog(
-                            //   context: context,
-                            //   builder: (context) => AlertDialog(
-                            //     content: Container(
-                            //       child: GuestMemeView(
-                            //         imgUrl:
-                            //             snapshot.data?.docs[index].get('url'),
-                            //         userName: snapshot.data?.docs[index]
-                            //             .get('Username'),
-                            //       ),
-                            //     ),
-                            //   ),
-                            // );
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                );
-        },
+                              image: searchresult[index],
+                            ),
+                            onTap: () async {
+                              Navigator.pop(context, searchresult[index]);
+                            },
+                          ),
+                        );
+                      },
+                    )
+                  //: Container(),
+                  : GridView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: MemeURL.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.all(3),
+                          child: InkWell(
+                            child: FadeInImage.memoryNetwork(
+                              fit: BoxFit.cover,
+                              placeholder: kTransparentImage,
+                              image: MemeURL[index],
+                            ),
+                            onTap: () async {
+                              Navigator.pop(context, MemeURL[index]);
+                            },
+                          ),
+                        );
+                      },
+                    ),
+            ),
+            // Flexible(
+            //   child: GridView.builder(
+            //     scrollDirection: Axis.vertical,
+            //     shrinkWrap: true,
+            //     itemCount: MemeURL.length,
+            //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //         crossAxisCount: 2),
+            //     itemBuilder: (context, index) {
+            //       return Container(
+            //         margin: EdgeInsets.all(3),
+            //         child: InkWell(
+            //           child: FadeInImage.memoryNetwork(
+            //             fit: BoxFit.cover,
+            //             placeholder: kTransparentImage,
+            //             image: MemeURL[index],
+            //           ),
+            //           onTap: () async {
+            //             Navigator.pop(context, MemeURL[index]);
+            //           },
+            //         ),
+            //       );
+            //     },
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _handleSearchStart() {
+    setState(() {
+      _isSearching = true;
+    });
+  }
+
+  void _handleSearchEnd() {
+    setState(() {
+      this.icon = Icon(
+        Icons.search,
+        color: Colors.white,
+      );
+      this.appBarTitle = Text(
+        "Search Sample",
+        style: TextStyle(color: Colors.white),
+      );
+      _isSearching = false;
+      _controller.clear();
+    });
+  }
+
+  void searchOperation(String searchText) {
+    searchresult.clear();
+    if (_isSearching != null) {
+      for (int i = 0; i < MemeName.length; i++) {
+        String name = MemeName[i];
+        String url = MemeURL[i];
+        if (name.toLowerCase().contains(searchText.toLowerCase())) {
+          searchresult.add(url);
+        }
+      }
+    }
   }
 }
